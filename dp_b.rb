bae7f78904ebabb9a51e1abf43f61bee7235771e
nobmurakita <nobmurakita@users.noreply.github.com>
2020-01-30 00:27:22 +0900

    B問題

---
+++ dp_b.rb
@@ -0,0 +1,12 @@
+# https://atcoder.jp/contests/dp/tasks/dp_b
+n, k = gets.split.map(&:to_i)
+h = gets.split.map(&:to_i)
+dp = Array.new(n, Float::INFINITY)
+dp[0] = 0
+(1...n).each do |i|
+  (1..[k, i].min).each do |j|
+    cost = dp[i-j] + (h[i] - h[i-j]).abs
+    dp[i] = cost if cost < dp[i]
+  end
+end
+puts dp.last
